cf1a9a2e615fc953f0567f0d3483458714fe3b8e
Esteban Borai <estebanborai@outlook.com>
2018-09-10 00:29:12 -0300

    Add docs base setup

---
+++ docs/bundle/index.js
@@ -0,0 +1,1 @@
+"use strict";var msg=document.getElementById("msg");msg.innerText="This website is under construction! You are welcome to contribute!";
---
+++ docs/src/scripts/base.js
@@ -0,0 +1,3 @@
+const msg = document.getElementById('msg');
+
+msg.innerText = 'This website is under construction! You are welcome to contribute!';
